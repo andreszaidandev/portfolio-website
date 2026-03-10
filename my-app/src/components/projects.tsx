7a5951bbc56a9e18664bb28a1dff7fe07177c0e0
andreszaidandev <andreszaidan02@gmail.com>
2026-03-10 14:31:29 -0400

    lmao

--- my-app/src/components/projects.tsx
+++ my-app/src/components/projects.tsx
@@ -5,6 +5,9 @@ import planOfStudyImage from "../assets/PlanOfStudy.jpg"
 import PersonalWebsiteImage from "../assets/PersonalWebsite.jpeg"
 import HandTrackingImage from "../assets/HandtrackingCamera.jpeg"
 import FinacialTrackerImage from "../assets/FinanceImage.png"
+import SpotifyWidgetImage from "../assets/SpotifyWidget.png"
+import TrainScheduleImage from "../assets/TrainSchedule.png"
+import ReadmeGenImage from "../assets/ReadmeAI.png"
 import "./projects.css"
 
 const projects: Testimonial[] = [
@@ -41,6 +44,36 @@ const projects: Testimonial[] = [
     href: "https://andreszaidan.com",
     github: "https://github.com/andreszaidandev/portfolio-website",
   },
+  {
+    name: "Spotify Widget",
+    designation: "Electron + JavaScript + HTML/CSS",
+    quote:
+      "A spotify widget built with Electron that displays the currently playing track, album art, and playback controls. The project involved integrating with the Spotify API, creating a retro UI, and ensuring smooth performance on the desktop.",
+    src: SpotifyWidgetImage,
+    github: "https://github.com/andreszaidandev/spotify-widget",
+  },
+  {
+    name: "Boston Train schedule",
+    designation: "Vite + React + Typescript",
+    quote:
+      "A web application for viewing and tracking Boston MBTA train schedules in real-time. Built with Vite and React, the project focused on creating a responsive interface and integrating with the MBTA API for accurate information.",
+    src: TrainScheduleImage,
+    href: "https://train-api-beta.vercel.app",
+    github: "https://github.com/andreszaidandev/train-api",
+  },
+  {
+    name: "ReadMe Ai Generator",
+    designation: "Vite + React + Typescript, Gemini Ai API, Supabase",
+    quote:
+      "An AI-powered tool for generating README files for GitHub repositories. Built with Vite and React, the project leveraged the Gemini AI API for natural language processing and Supabase for database management, GitHub authentication and repository data fetching.",
+    src: ReadmeGenImage,
+    href: "https://readme-gen-eta.vercel.app",
+    github: "https://github.com/andreszaidandev/Readme-Gen",
+  },
+
+
+  
+  
 ]
 
 export default function ProjectsSection() {
